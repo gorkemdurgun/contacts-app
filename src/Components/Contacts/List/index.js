66e6a51e47e512074998d03215c5f05fd9360db2
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-07-13 11:55:31 +0300

    css, filtered list

--- src/Components/Contacts/List/index.js
+++ src/Components/Contacts/List/index.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function List({ contacts }) {
+    const [filterText, setFilterText] = useState('');
+
+    const filteredContacts = contacts.filter((item) => {
+        return Object.keys(item).some((key) => {                    // objenin keylerini al (full_name ve phone_number)
+            return item[key]
+                .toString()                                         // string'e çevir  
+                .toLowerCase()                                      // küçük harf yap
+                .includes(filterText.toLocaleLowerCase());          // contacts listesinden eşleşenleri al
+        })
+    });
+
+    //console.log(filteredContacts);
+
     return (
         <div>
-            <ul>
+
+            <h5>Total contact: {filteredContacts.length}</h5>
+            <input
+                type="text"
+                placeholder='Search'
+                value={filterText}
+                onChange={(e) => setFilterText(e.target.value)} />
+
+            <ul className='List'>
                 {
-                    contacts.map((contact, index)=> (
-                        <li key={index}> {contact.full_name} </li>
+                    filteredContacts.map((item, index) => (
+                        <li key={index}>
+                            <span> {item.full_name} </span>
+                            <span> {item.phone_number} </span>
+                        </li>
                     ))
                 }
             </ul>
--- src/Components/Contacts/index.js
+++ src/Components/Contacts/index.js
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import Form from './Form';
 import List from './List';
+import '../../App.css'
 
 function Contacts() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            full_name: 'Görkem',
+            phone_number: 1234,
+        },
+        {
+            full_name: 'Bryan',
+            phone_number: 9845,
+        },
+        {
+            full_name: 'Valron',
+            phone_number: 3496,
+        },
+    ]);
 
     useEffect(() => {
         console.log(contacts);
     }, [contacts])
 
     return (
-        <div>
-            <List contacts={contacts} />
+        <div className='Container'>
+            <h2> Add Contact </h2>
             <Form addContact={setContacts} contacts={contacts} />
+            <hr />
+            <h2> Contacts </h2>
+            <List contacts={contacts} />
         </div>
     )
 }
